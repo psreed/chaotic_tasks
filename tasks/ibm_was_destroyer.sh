c3b16f5b2326f8d8e0d3aa9fa375f823b0f1bbd1
Paul Reed <paul@paulreed.ca>
2018-04-09 19:31:03 -0400

    added some output

--- tasks/ibm_was_destroyer.sh
+++ tasks/ibm_was_destroyer.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 if [[ $PT_iamsure == "YES" ]]; then
   if [[ $PT_iamreallysure == "YES" ]]; then
+    echo "Removing /opt/IBM"
     rm -rf /opt/IBM
+    echo "Removing /var/ibm"
     rm -rf /var/ibm
+    echo "Removing wsadmin user (including home directory)"
     rm -rf /home/wsadmin
     userdel wsadmin
     groupdel wsadmins
