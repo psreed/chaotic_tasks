e5558f1d18cf77d1a17c026515411d5c4a60b96f
Paul Reed <paul@paulreed.ca>
2018-10-26 22:25:49 -0400

    uptime task test

---
+++ tasks/uptime.ps1
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function DoBashThings {
+    uptime
+}
+
+"DoBashThings"  # This runs the bash script, in PS it's just a string
+"exit"          # This quits the bash version, in PS it's just a string
+
+
+# PowerShell code here
+# --------------------
+net statistics workstation
