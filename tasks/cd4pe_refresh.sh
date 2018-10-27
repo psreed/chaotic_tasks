8dc83005d15820325c1098bc6e2c893bd738e9e1
Paul Reed <paul@paulreed.ca>
2018-10-26 21:49:49 -0400

    testing

--- tasks/cd4pe_refresh.sh
+++ tasks/cd4pe_refresh.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 
 #Test for docker service
-if [[ "`/usr/bin/which docker 2>&1 | grep "no docker in"`" =~ "docker" ]]; then 
-  echo "No docker on selected host."
+DOCKER=`/usr/bin/which docker 2>/dev/null`
+if [ -z $DOCKER ]; then echo "No docker on selected host."; exit 1; fi
+
+#docker stop & remove image
+IMG=`docker image list | awk '/puppet\/continuous-delivery-for-puppet-enterprise/ {print $3}'`
+if [ -z $IMG]; then echo "Could not get image ID."; echo $IMG; exit 1; fi
+
+CON=`docker container list | awk '/puppet\/continuous-delivery-for-puppet-enterprise/ && /\/sbin\/init/ && /cd4pe/ {print $1}'`
+if [ -z $CON]; then echo "Could not get container ID."; echo $CON; exit 1; fi
+
+C=`docker stop $CON 2>&1`
+if [[ "$C" != "$CON" ]]; then 
+  echo "Could not stop container for continuous delivery for puppet enterprise."
+  echo $CON
+  echo $C
   exit 1
 fi
-
-#Test for cd4pe container
-if ! [[ "`docker image list | grep puppet/continuous-delivery-for-puppet-enterprise`" =~ "continuous-delivery-for-puppet-enterprise" ]]; then 
-  echo "Continuous Delivery for Puppet Enterprise container not found on selected host"
+I=`docker rmi $IMG 2>&1`
+if [[ "$I" != "$IMG" ]]; then 
+  echo "Could not remove image for continuous delivery for puppet enterprise container."
+  echo $IMG
+  echo $I
   exit 1
 fi
-
-#stop the container
-docker stop puppet/continuous-delivery-for-puppet-enterprise
-
-#docker remove image
-docker rm puppet/continuous-delivery-for-puppet-enterprise
-docker rmi puppet/continuous-delivery-for-puppet-enterprise
 
 #pull latest
 docker pull puppet/continuous-delivery-for-puppet-enterprise:latest
